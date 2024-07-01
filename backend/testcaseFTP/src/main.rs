3f7c0cfb30e87a4c9c243a778c8f6c910016dc98
Yollotl Tamayo <yollotltamayo@comunidad.unam.mx>
2024-07-01 10:52:27 -0600

    Initial commit

--- backend/testcaseFTP/src/main.rs
+++ backend/testcaseFTP/src/main.rs
@@ -1,11 +1,14 @@
+use libunftp::Server;
 use unftp_sbe_fs::ServerExt;
 
 #[tokio::main]
 pub async fn main() {
     let ftp_home = "/Users/yollotl/guapa/judge/juez_guapa/backend/testfile/resources/";
-    let server = libunftp::Server::with_fs(ftp_home)
+    let server = Server::with_fs(ftp_home)
         .greeting("Welcome to my FTP server")
-        .passive_ports(50000..65535);
+        .passive_ports(50000..65535)
+        .build()
+        .unwrap();
 
     server.listen("127.0.0.1:2121").await;
 }
